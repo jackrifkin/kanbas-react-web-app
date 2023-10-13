d686f60eff3a69f8e2322b468eea975eacb89ed5
Jack Rifkin <jack.rifkin@icloud.com>
2023-10-12 23:21:15 -0400

    lab 3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import Labs from "./Labs";
+import HelloWorld from "./Labs/a3/HelloWorld";
 import './App.css';
+import Kanbas from "./Kanbas";
+import { HashRouter } from "react-router-dom";
+import { Routes, Route } from "react-router";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <HashRouter>
+        <HelloWorld />
+        <Kanbas />
+        <Labs />
+        <Routes>
+          <Route path="/hello" element={<HelloWorld />} />
+          <Route path="/Labs/*" element={<Labs />} />
+          <Route path="/Kanbas/*" element={<Kanbas />} />
+        </Routes>
+      </HashRouter>
     </div>
   );
 }
